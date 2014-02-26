08808efbb4644adb593c0aa41d4f17ac713de375
SirAnthony <anthony@adsorbtion.org>
2014-02-26 17:08:59 +0600

    Done.

--- app.js
+++ app.js
@@ -10,8 +10,11 @@ W=a.width,
 H=a.height,
 j=120,
 u=65;
+v=83;
+z=68;
+_=['#FFF','#FF3','red'];
 $=eval;
-function O(w,h,x,y,X,F,r,Y){
+function O(w,h,x,y,X,r,F){
 	var s={
 		v:X,V:0,
 		z:w,Z:h,
@@ -20,27 +23,29 @@ function O(w,h,x,y,X,F,r,Y){
 	s.T=function(t){
 			s.p += s.v*t;
 			s.P += s.V*t;
-			s.p <- w && (s==P?s.p=-w+1:s.E());
-			Y &&
+			s.p < -w && (s==P?s.p=-w+1:s.E());
+			F == 1 &&
 				P.l>0 && Z(P.p-s.p)<w/2+P.z/2 &&
 				Z(P.P-s.P)<h/2+P.Z/2 &&
 				(P.E() || s.E());
-			s == P && s.p > W-w && (s.p = W - w) || (s.P < -h || s.P > H) && s.E();
-			c[f] = F;
+			F == 2 && s.p > W-w && (s.p = W - w) || (s.P < -h || s.P > H) && s.E();
+			c[f] = _[F];
 			c[J](s.p,s.P,w,h);
+			c[f] = '#06F';
+			F && c[J](s.p+v,s.P+5,9,u-9)
 			c[G]()
 	}
 	s.E=function(){
-			s==P && 0 < --s.l && (s.p=x,s.P=y) ||
+			F == 2 && 0 < --s.l && (s.p=x,s.P=y) ||
 			r.splice(r.indexOf(s),1);
-			0 < P.l && P.s++
+			0 < P.l && (P.s+=P.p/9)
 	}
 	return s;
 };
 M=[A=[],B=[]];
-B[q](P=new O(j,u,10,300,0,'red',B));
+B[q](P=new O(j,u,9,300,0,B,2));
 C=function(d) {
-	v=83,F=87,z=68;
+	F=87;
 	return function(e){
 		k = e.keyCode;
 		P.v = d * (k - z ? k - u ? 0 : -1 : 1);
@@ -50,11 +55,11 @@ C=function(d) {
 b.onkeydown = C(1);
 b.onkeyup = C(0);
 N = Date.now;
-S=N();
-P.s=P.l=3;
-R=Math.random;
-rd='new O(j,8,W,u+j*A[V],-0.4,\'#FFF\',A);';
-E = 'if(!A[V])while(A[V]<5)A[q]($(rd));S%R()>0.8&&B[V]<8&&B[q](new O(j,u,W,R()*H,-R(),\'#FF3\',B,1))';
+S = N();
+P.s = P.l = 3;
+R = Math.random;
+r = 'new O(j,8,W,u+j*A[V],-0.4,A,0)';
+E = 'if(!A[V])while(A[V]<5)A[q]($(r));S%R()>0.8&&B[V]<8&&B[q](new O(j,u,W,R()*H,-R(),B,1))';
 c.font='35px Sans';
 ~function L(){
 	requestAnimationFrame(L);
@@ -67,7 +72,7 @@ c.font='35px Sans';
 	c[G]();
 	p=function(o){ o.T(D) }
 	A[U](p);
-	c[Q]('♥'+P.l,0,u);
-	0 >= P.l && c[Q]('Score: '+P.s,W/2.5,250);
+	c[Q]('♥'+P.l,9,40);
+	0 >= P.l && c[Q]('Score: '+~~P.s,W/2.5,250);
 	B[U](p);
 }()
